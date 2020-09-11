801b806037be63b7f8c9ffec61bff14a1f9bb10e
Moss Normand <none>
2020-09-10 20:17:39 -0400

    simplifying pthreads example

--- pthreads_ex.c
+++ pthreads_ex.c
@@ -4,50 +4,21 @@
 #include <unistd.h> 
 #include <pthread.h> 
 
-enum arithematic_operations{ADDITION,DIVISION,MULTIPLICATION}; 
-
 int GLOBAL = 0; 
-
-typedef struct {
-
-enum arithematic_operations operation; 
-
-pthread_t *tid; 
-
-}ThreadArgs;
   
-
-
-  
-void *workerThreadFunc(void *args) 
+void *workerThreadFunc(void *arg) 
 { 
 
     while ( GLOBAL < 10){
     	GLOBAL++; 
     	}; 
 
-    ThreadArgs * args_struct = (ThreadArgs *)args; 
-    long *tid = (long *) args_struct->tid;
+    long *tid = (long *) arg;
     static int shared_var = 0; 
-    
-    printf("%d - OPERATION\n", args_struct->operation);
-    if(args_struct->operation == ADDITION){
-    	printf("ADDITION! - Thread ID %ld \n", *tid);
-    	++shared_var; 
-    	}; 
-    if(args_struct->operation == DIVISION){
+    int local_var = 0; 
 
-    	printf("DIVISION! - Thread ID %ld \n", *tid);
-       shared_var = shared_var * shared_var; 
-       shared_var = shared_var / 2; 
-       }
-    if(args_struct->operation == MULTIPLICATION) {
-    	printf("MULTIPLICATION! - Thread ID %ld \n", *tid); 
-    	shared_var * 10; 
-    	}; 
-
-    printf("Global Var Value: %d, Static Var Value: %d, Thread ID: %ld, \n", 
-    ++GLOBAL, ++shared_var, *tid); 
+    printf("Global Var Value: %d, Static Var Value: %d, Local Var Value: %d, Thread ID: %ld, \n", 
+    ++GLOBAL, ++shared_var, ++local_var, *tid); 
 } 
   
 int main() 
@@ -61,11 +32,7 @@ int main()
 
     for (int i = 0; i < 3; i++) {
     
-    	ThreadArgs * args = (ThreadArgs *) malloc(sizeof(ThreadArgs)); 
-	args->tid = pthreads[i]; 
-	args->operation = i; 
-	//pthread_create(pthreads[i], NULL, workerThreadFunc, (void *)pthreads[i]); 
-        pthread_create(args->tid, NULL, workerThreadFunc, (void *)args); 
+	pthread_create(pthreads[i], NULL, workerThreadFunc, (void *)pthreads[i]); 
         
         }; 
   
